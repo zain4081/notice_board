2f586b1353de6dda56a626da69229602d4f26d0f
ZAIN ABBAS <126979431+zain4081@users.noreply.github.com>
2024-02-04 22:24:31 +0500

    Update about.php

--- about.php
+++ about.php
@@ -2,11 +2,7 @@
 <h3>The Online Notice Board System Which take place and Wall-Notice Board to digitalize our system. And make notification system easier and more efficient.</h3>
 <div class="col-sm-4">
 	<h6 style="color:grey;">Authors</h6>
-	<h4>BC170401542     BS150403469</h4>
+	<h4>ZAIN ABBAS</h4>
 </div>
 <div class="col-sm-4"></div>
-<div class="col-sm-4">
-	<h6 style="color:grey;">Supervisor</h6>
-	<h4>Miss Komal Khawer</h4>
-</div>
 </div>
